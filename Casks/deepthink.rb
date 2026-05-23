7cef1cf5c0b2ebfd4d765a2f4bbd3d51046fcb0c
Aagam Jain <aagamjain573@gmail.com>
2026-05-24 02:18:45 +0530

    chore: update SHA256 for v1.0.0 release asset rebuild

--- Casks/deepthink.rb
+++ Casks/deepthink.rb
@@ -1,6 +1,6 @@
 cask "deepthink" do
   version "1.0.0"
-  sha256 "ecadfb25b1c569ee37fed25adf7d69eb080c83c6c6640bf28507302b28c08e77"
+  sha256 "e27cbca5eaea9001defbcf886a49517e3dfab40bc69b3c2e5a30488ad71fdbea"
 
   url "https://github.com/getlost01/deepthink/releases/download/v#{version}/DeepThink-#{version}.zip"
   name "DeepThink"
